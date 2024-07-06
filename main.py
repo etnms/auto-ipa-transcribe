aeec27ba641d9ea3f24d62cc7963ff086ee14b64
etnms <etiennenemes@gmail.com>
2024-07-06 19:27:09 +0200

    added speech to text functions and output writing

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import eng_to_ipa as ipa
+from transcribe_audio import transcribe_audio, get_large_audio_transcription_on_silence
 
-test_string = ipa.convert("The quick brown fox jumped over the lazy dog.")
-print(test_string)
+
+text = get_large_audio_transcription_on_silence("files/test2.wav")
+# text = transcribe_audio("files/test_sample.wav")
+ipa_text = ipa.convert(text)
+print(ipa_text)
+
+# create an output with the IPA transcription
+with open('output.txt', 'w', encoding='utf-8') as output_file:
+    output_file.write(ipa_text)
