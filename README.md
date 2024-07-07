6c98402c9c52ea8bccae28b11bd03cbbcac38d32
etnms <etiennenemes@gmail.com>
2024-07-07 21:38:01 +0200

    started added functions to create elan file, added basic readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# Auto IPA transcription
+
+## Goals of the project
+
+## Features
+
+## Stack used
--- create_elan_file.py
+++ create_elan_file.py
@@ -1,15 +1,19 @@
 import pympi
 
 
-def create_empty_elan_file(output_file):
+def create_empty_elan_file(output_file, language_code):
+    '''
+    Function to create default Elan file, with output file name as parameter, and 
+    language code for tier name (ex en or English)
+    '''
     # Create a new EAF object
     eaf = pympi.Elan.Eaf()
 
-    # Add a default linguistic type
-    eaf.add_linguistic_type('default-lt')
+    # Add linguistic type
+    eaf.add_linguistic_type('transcription')
 
     # Add an empty tier
-    eaf.add_tier('default')
+    eaf.add_tier(f'Transcription ({language_code})', ling='transcription')
 
     # Save the EAF to file
     eaf.to_file(output_file)
@@ -17,5 +21,12 @@ def create_empty_elan_file(output_file):
     print(f"Empty ELAN file created: {output_file}")
 
 
+def add_annotations():
+    '''
+    Function to automatically add annotations for transcription and IPA
+    '''
+    pass
+
+
 # Usage
-create_empty_elan_file("empty_elan_file.eaf")
+create_empty_elan_file("empty_elan_file.eaf", "en")
