63a327f41d76e2039f4ca9800e859e8ce4d58575
etnms <etiennenemes@gmail.com>
2024-07-11 16:08:44 +0200

    added base function to convert videos to audio

---
+++ video_to_audio.py
@@ -0,0 +1,8 @@
+from audio_extract import extract_audio
+
+
+input_path = "./files/test_video.mov"
+output_path = "./files/test_video_audio.mp3"
+
+extract_audio(input_path=input_path,
+              output_path=output_path)
