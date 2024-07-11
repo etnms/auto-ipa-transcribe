affed726493633dc63b8646da28b4016185618bb
etnms <etiennenemes@gmail.com>
2024-07-11 19:15:23 +0200

    started adding video functions in main.py

--- main.py
+++ main.py
@@ -2,14 +2,27 @@ import eng_to_ipa as ipa
 from transcribe_audio import transcribe_audio, transcribe_large_audio_file
 from write_to_file import write_text
 from create_elan_file import create_empty_elan_file
+from video_to_audio import convert_video_to_audio
+from pathlib import Path
 
 
 # Audio file to work with
-audio_file = "files/test_sample.wav"
+input_file = "files/test_video.mov"
+video_formats = [".mp4", ".mkv", ".webm",
+                 ".flv", ".avi", ".mov", ".wnv", ".m4v"]
 
+# Convert input_file to a Path object
+file_path = Path(input_file)
+
+# Check if the file suffix (extension) is in the video_formats list
+if file_path.suffix.lower() in video_formats:
+    output_path = file_path.with_suffix('.wav')
+    input_file = convert_video_to_audio(str(file_path), str(output_path))
+
+'''
 
 text, ipa_text, chunks_information = transcribe_large_audio_file(
-    audio_file)
+    input_file)
 
 # text = transcribe_audio("files/test_sample.wav")
 
@@ -21,4 +34,5 @@ write_text("outputs", "output_ipa.txt", ipa_text)
 
 # Usage
 create_empty_elan_file("empty_elan_file.eaf", "en",
-                       audio_file, chunks_information)
+                       input_file, chunks_information)
+'''
--- video_to_audio.py
+++ video_to_audio.py
@@ -4,5 +4,12 @@ from audio_extract import extract_audio
 input_path = "./files/test_video.mov"
 output_path = "./files/test_video_audio.mp3"
 
-extract_audio(input_path=input_path,
-              output_path=output_path)
+# Supported audio formats : WAV, OGG, MP3, AAC, FLAC, M4A, OGA, OPUS
+
+# Supported video formats : MP4, MKV, WEBM, FLV, AVI, MOV, WMV, M4V
+
+
+def convert_video_to_audio(input_path, output_path):
+    # return file as wav to keep quality
+    audio_file = extract_audio(input_path, output_path, "wav")
+    return audio_file
